21afea34207a873b28da88f57ec85360eb2294d3
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-06-28 03:24:22 +0800

    Created UI for PostsScreen groups picker and updated AddPostScreen.js

---
+++ App/components/AppPicker.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function AppPicker({ value, onValueChange, optionList }) {
+  return (
+    <View style={styles.container}>
+      <Picker
+        style={styles.picker}
+        selectedValue={value}
+        onValueChange={onValueChange}
+      >
+        {optionList.map((name, index) => {
+          return <Picker.Item label={name} value={name} key={index} />;
+        })}
+      </Picker>
+      <MaterialCommunityIcons
+        name="chevron-down"
+        size={35}
+        color={colors.medium}
+        style={styles.pickerIcon}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    width: "85%",
+    paddingHorizontal: 15,
+    paddingVertical: 2,
+    borderRadius: 25,
+    flexDirection: "row",
+  },
+  picker: {
+    backgroundColor: colors.white,
+    width: "100%",
+  },
+  pickerIcon: {
+    alignSelf: "center",
+    position: "absolute",
+    right: 15,
+  },
+});
+
+export default AppPicker;
--- App/screens/PostsScreen.js
+++ App/screens/PostsScreen.js
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from "react";
-import { StyleSheet, FlatList, RefreshControl } from "react-native";
+import React, { useState, useCallback, useContext } from "react";
+import { StyleSheet, FlatList, RefreshControl, View } from "react-native";
 import { firebase } from "../../firebase/config";
 import { useFocusEffect } from "@react-navigation/native";
+import Picker from "../components/AppPicker";
 
 import ListItem from "../components/lists/ListItem";
 import ListItemSeparator from "../components/lists/ListItemSeparator";
@@ -9,13 +10,21 @@ import Screen from "../components/Screen";
 import colors from "../config/colors";
 import AddPostButton from "../components/AddPostButton";
 import routes from "../navigation/routes";
+import AuthContext from "../auth/context";
 
 function PostsScreen({ navigation }) {
   const [events, setEvents] = useState([]);
+  const [selectedEvents, setSelectedEvents] = useState([]);
+  const [group, setGroup] = useState("");
+  const [groupList, setGroupList] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const { user } = useContext(AuthContext);
 
   const eventsRef = firebase.firestore().collection("events");
-  const refresh = () => {
+  const groupsRef = firebase.firestore().collection("groups");
+  const userRef = firebase.firestore().collection("users").doc(user.id);
+
+  const refreshEvents = (groupName) => {
     setRefreshing(true);
     eventsRef
       .orderBy("score")
@@ -24,20 +33,85 @@ function PostsScreen({ navigation }) {
         const newEvents = [];
         snapshot.docs.forEach((doc) => {
           const event = doc.data();
-          event.id = doc.id;
           newEvents.push(event);
         });
         setEvents(newEvents.reverse());
       })
       .catch((error) => alert(error));
+
+    // groupsRef
+    //   .doc(groupName)
+    //   .get()
+    //   .then((groupDoc) => {
+    //     const groupEventIds = groupDoc.data().events;
+    //     const displayEventIds = filterArray(groupEventIds, selectedEvents);
+    //     const newEvents = [];
+    //     displayEventIds.forEach((eventId) => {
+    //       eventsRef
+    //         .doc(eventId)
+    //         .get()
+    //         .then((doc) => {
+    //           const event = doc.data();
+    //           newEvents.push(event);
+    //         })
+    //         .catch((error) => alert(error));
+    //     });
+    //     newEvents.sort(compareEvents);
+    //     console.log(newEvents);
+    //     setEvents(newEvents);
+    //   })
+    //   .catch((error) => alert(error));
     setRefreshing(false);
   };
+  const fetchUserData = () => {
+    userRef
+      .get()
+      .then((userDoc) => {
+        const data = userDoc.data();
+        const newGroups = data.group;
+        const newSelectedEvents = data.selectedEvents;
+        setGroupList(newGroups);
+        setSelectedEvents(newSelectedEvents);
+      })
+      .catch((error) => alert(error));
+  };
 
-  useFocusEffect(useCallback(refresh, []));
+  const filterArray = (inArray, notInArray) => {
+    const condition = (item) => {
+      return !notInArray.includes(item);
+    };
+    const result = inArray.filter(condition);
+    return result;
+  };
+  const compareEvents = (a, b) => {
+    if (a.score > b.score) {
+      return -1;
+    } else if (a.score < b.score) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  const onPickerChange = (itemValue) => {
+    setGroup(itemValue);
+    refreshEvents(itemValue);
+  };
+
+  useFocusEffect(useCallback(fetchUserData, []));
 
   return (
     <Screen style={styles.container}>
-      <AddPostButton onPress={() => navigation.navigate(routes.ADD_POST)} />
+      <View style={styles.headerBar}>
+        <Picker
+          value={group}
+          onValueChange={onPickerChange}
+          optionList={groupList}
+        />
+        <AddPostButton
+          onPress={() => navigation.navigate(routes.ADD_POST, { group })}
+        />
+      </View>
       <FlatList
         data={events}
         keyExtractor={(event) => event.id.toString()}
@@ -45,7 +119,7 @@ function PostsScreen({ navigation }) {
         refreshControl={
           <RefreshControl
             refreshing={refreshing}
-            onRefresh={refresh}
+            onRefresh={() => refreshEvents(group)}
             colors={[colors.primary]}
           />
         }
@@ -55,6 +129,7 @@ function PostsScreen({ navigation }) {
             dateTime={item.dateTime.toDate()}
             score={item.score}
             id={item.id}
+            onAdd={() => refreshEvents(group)}
           />
         )}
       />
@@ -66,6 +141,11 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     backgroundColor: colors.light,
+  },
+  headerBar: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 20,
   },
 });
 
