991b1e6bba0921778562816c229f6483ccecbc45
ErnestCuong <sendmailtocuong@gmail.com>
2021-07-24 04:47:54 +0800

    Create FollowButton.js

---
+++ App/components/lists/FollowButton.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import colors from "../../config/colors";
+import Text from "../Text";
+import AuthContext from "../../auth/context";
+import { firebase } from "../../../firebase/config";
+
+function FollowButton({ title }) {
+  const { user } = useContext(AuthContext);
+  const [follow, setFollow] = useState(true);
+
+  const usersRef = firebase.firestore().collection("users").doc(user.id);
+
+  const handleFollow = async () => {
+    setFollow(true);
+    const addGroup = await usersRef
+      .update({
+        groups: firebase.firestore.FieldValue.arrayUnion(title),
+      })
+      .catch((error) => alert(error));
+  };
+  const handleUnfollow = async () => {
+    setFollow(false);
+    const removeGroup = await usersRef
+      .update({
+        groups: firebase.firestore.FieldValue.arrayRemove(title),
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        { backgroundColor: follow ? colors.primary : colors.white },
+      ]}
+      onPress={follow ? handleUnfollow : handleFollow}
+    >
+      <Text style={{ color: follow ? colors.light : colors.medium }}>
+        {follow ? "Following" : "Follow"}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  containter: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    justifyContent: "flex-start",
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+    backgroundColor: "grey",
+  },
+  title: {
+    width: 150,
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    width: 100,
+    borderRadius: 20,
+  },
+});
+export default FollowButton;
